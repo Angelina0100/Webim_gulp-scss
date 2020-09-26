2b112197c1438e9d1951826510c5bd73d1f77338
Angelina0100 <angelinae259@gmail.com>
2020-09-27 01:33:51 +0300

    Initial commit

---
+++ app/js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.header-burger').on('click', function() {
+        $('.header-menu').slideToggle(300, function() {
+            if ($(this).css('display') === 'none') {
+                $(this).removeAttr('style');
+            }
+        });
+    });
+})
